2cbcc3aac7295a9539c7b737ae6b7972b2f060ca
SydUNC <sydneyg@live.unc.edu>
2022-09-26 17:21:58 -0400

    Added timezone compatibility

--- cli.js
+++ cli.js
@@ -6,12 +6,14 @@ var argv = process.argv.slice(2);
 
 // Time zone guess
 var timezone = moment.tz.guess();
+console.log(timezone);
+// Time zone if user input
 if (process.argv.indexOf('-z') > -1) {
-	timezone = moment.tz(argv[process.argv.indexOf('-z') - 1]);
+	timezone = argv[process.argv.indexOf('-z') - 1];
 }
 var latitude = 0;
 var longitude = 0;
-console.log(argv);
+
 // Help command
 if (process.argv.indexOf('-h') > -1) {
 	  console.log("Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
@@ -32,7 +34,7 @@ if (process.argv.indexOf('-w') > -1) {
 	longitude = argv[process.argv.indexOf('-w') - 1];
 }
 // Fetch URL
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&timezone=auto');
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&timezone='+timezone);
 
 // Get json
 const data = await response.json();
@@ -42,31 +44,34 @@ if (process.argv.indexOf('-j') > -1) {
 }
 
 // Get days and parse precipitation info
-var days = 0;
+var days = 1;
 
 if (process.argv.indexOf('-d') > -1) {
 	days = argv[process.argv.indexOf('-d') - 1];
 }
 
 if (days == 0) {
-	  if (data.daily.precipitation_hours[days] < 0) {
+	  if (data.daily.precipitation_hours[days] == 0) {
 		  console.log('You will not need your galoshes')
 	  } else {
 		  console.log('You might need your galoshes')
 	  }
           console.log('today.')
+	  process.exit(0);
 } else if (days > 1) {
-	  if (data.daily.precipitation_hours[days] < 0) {
+	  if (data.daily.precipitation_hours[days] == 0) {
 		  console.log('You will not need your galoshes')
 	  } else {
 		  console.log('You might need your galoshes')
 	  }
 	  console.log("in " + days + " days.")
+	  process.exit(0)
 } else {
-	if (data.daily.precipitation_hours[days] < 0) {
+	if (data.daily.precipitation_hours[days] == 0) {
 		console.log('You will not need your galoshes')
 	} else {
 		console.log('You might need your galoshes')
 	}
 	  console.log("tomorrow.")
+	  process.exit(0)
 }
